915455f4c785b6368d96a35077cc3e05087a15d0
PunithArtham <apunithnlg19@gmail.com>
2025-04-21 12:56:54 +0530

    Update jwtToken.js

--- BACKEND/utils/jwtToken.js
+++ BACKEND/utils/jwtToken.js
@@ -1,16 +1,15 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken(); // Generate JWT
 
-  // Ensure COOKIE_EXPIRE is a number and calculate the expiration in milliseconds
   const expiresIn = process.env.COOKIE_EXPIRE ? Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000 : 7 * 24 * 60 * 60 * 1000; // Default to 7 days if COOKIE_EXPIRE is not set
-  const cookieExpireDate = new Date(Date.now() + expiresIn); // Calculate the expiration date as a Date object
+  const cookieExpireDate = new Date(Date.now() + expiresIn);
 
   res
     .status(statusCode)
     .cookie("token", token, {
-      expires: cookieExpireDate,  // Set the expires option to the calculated Date object
-      httpOnly: true,  // Ensures cookie is not accessible via JavaScript
-      secure: process.env.NODE_ENV === "production",  // Only send cookie over HTTPS in production
+      expires: cookieExpireDate,  
+      httpOnly: true,  
+      secure: process.env.NODE_ENV === "production",
     })
     .json({
       success: true,
